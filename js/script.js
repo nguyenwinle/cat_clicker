5a905d69688993fb61f778a63dd01f6525a6bf97
Nguyen Le <ucnguyenle@gmail.com>
2017-04-12 15:01:59 -0700

    refactor: Change to production code
    Instead of one cat, I added 2 cats that increments by one each time a picture is clicked which shows the number of increments on the top.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-var counter = 0;
-$("#my_elem").click(function(e) {
-    counter++;
-    alert (counter);
+$('#my_elem').click(function() {
+    $('#output').html(function(i, val) { return val*1+1 });
 });
+
+$('#elem2').append('<img src = "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496">');
+
+var $body = $('body');
+var $title = $('.title');
+var $title2 = $('.title2');
+
+$title.text("Cat1");
+$title2.text("Cat2");
