574c40b271f50441929c0621e73c6469e1fbe270
Nguyen Le <ucnguyenle@gmail.com>
2017-04-13 17:24:31 -0700

    refactor: made change to production code
    Change code to display pizza when button is clicked and remove pizza when pizza is clicked

--- js/script.js
+++ js/script.js
@@ -1,12 +1,76 @@
-$('#my_elem').click(function() {
-    $('#output').html(function(i, val) { return val*1+1 });
-});
+$(function() {
 
-$('#elem2').append('<img src = "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496">');
+    var data = {
+        lastID: 0,
+        pizzas: []
+    };
 
-var $body = $('body');
-var $title = $('.title');
-var $title2 = $('.title2');
 
-$title.text("Cat1");
-$title2.text("Cat2");
+    var octopus = {
+        addPizza: function() {
+            var thisID = ++data.lastID;
+
+            data.pizzas.push({
+                id: thisID,
+                visible: true
+            });
+            view.render();
+        },
+
+        removePizza: function(pizza) {
+            var clickedPizza = data.pizzas[ pizza.id - 1 ];
+            clickedPizza.visible = false;
+            view.render();
+        },
+
+        getVisiblePizzas: function() {
+            var visiblePizzas = data.pizzas.filter(function(pizza) {
+                return pizza.visible;
+            });
+            return visiblePizzas;
+        },
+
+        init: function() {
+            view.init();
+        }
+    };
+
+
+    var view = {
+        init: function() {
+            var addPizzaBtn = $('.add-pizza');
+            addPizzaBtn.click(function() {
+                octopus.addPizza();
+            });
+
+            // grab elements and html for using in the render function
+            this.$pizzaList = $('.pizza-list');
+            this.pizzaTemplate = $('script[data-template="pizza"]').html();
+
+            // Delegated event to listen for removal clicks
+            this.$pizzaList.on('click', '.remove-pizza', function(e) {
+                var pizza = $(this).parents('.pizza').data();
+                octopus.removePizza(pizza);
+                return false;
+            });
+
+            this.render();
+        },
+
+        render: function() {
+            // Cache vars for use in forEach() callback (performance)
+            var $pizzaList = this.$pizzaList,
+                pizzaTemplate = this.pizzaTemplate;
+
+            // Clear and render
+            $pizzaList.html('');
+            octopus.getVisiblePizzas().forEach(function(pizza) {
+                // Replace template markers with data
+                var thisTemplate = pizzaTemplate.replace(/{{id}}/g, pizza.id);
+                $pizzaList.append(thisTemplate);
+            });
+        }
+    };
+
+    octopus.init();
+}());
